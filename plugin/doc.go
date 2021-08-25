ede7bf13c5052c4e774763c51ccdccd2688c5e6f
lushaojie <lushaojie@bytedance.com>
2021-08-25 15:09:36 +0800

    docs: improve README.md && plugin/doc.go

--- plugin/doc.go
+++ plugin/doc.go
@@ -24,5 +24,21 @@
 // the binary protocol, too. The plugin can use the exit status to indicate whether
 // it finishes its jobs successfully.
 //
+// The response of a plugin may contains one or more `Generated` contents. Each content
+// can either be a single file -- when its `Name` is set and `InsertionPoint` is not set,
+// or a code segment to be inserted into a file which the `Name` field specifies.
+//
+// An **insertion point** is a position in a file that a code segment will be inserted
+// before. Sequential segments being inserted to a same point will keep their order.
+// The representation of an insertion point in a file is a string with a special format:
+//     "@@thriftgo_insertion_point(NAME)"
+// Where NAME is the name of the insertion point that the `InsertionPoint` of a `Generated`
+// could use.
+//
+// There will be some **pre-defined** insertion points for each backend language. Check the
+// code templates of that language to find out their names.
+//
+// All insertion points in the file will be erased before thriftgo finally writes out files.
+//
 // Refer to protocol.thrift for more information.
 package plugin
