0b038dd1ec61f14837304615cb0aab4c1ad72ffd
HeyJavaBean <imlehr@163.com>
2022-11-29 20:13:43 +0800

    feat: add code ref generator

---
+++ generator/golang/code_ref.go
@@ -0,0 +1,53 @@
+// Copyright 2022 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golang
+
+import (
+	"os"
+
+	"github.com/cloudwego/thriftgo/generator/golang/templates/ref"
+	"github.com/spf13/viper"
+)
+
+var refMap = map[string]string{}
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	config := viper.New()
+	config.SetConfigName("idl-ref")
+	config.AddConfigPath(wd)
+	config.SetConfigType("yml")
+	err = config.ReadInConfig()
+	if err != nil {
+		return
+	}
+	refMap = config.GetStringMapString("ref")
+}
+
+func DoRef(path string) (bool, string) {
+	if refpath := refMap[path]; refpath != "" {
+		return true, refpath
+	}
+	return false, ""
+}
+
+func TemplatesRef() []string {
+	return []string{
+		ref.FileRef,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cloudwego/thriftgo
 
 go 1.13
+
+require github.com/spf13/viper v1.14.0
