7e7ba0f7e1d583008d84c61b98728986fcaeabaa
HeyJavaBean <imlehr@163.com>
2022-12-01 14:32:00 +0800

    chore: change version tag

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/cloudwego/thriftgo
 
 go 1.13
-
-require github.com/spf13/viper v1.14.0
--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 )
 
 // Version of thriftgo.
-const Version = "0.2.3"
+const Version = "0.2.4"
 
 var (
 	a Arguments
