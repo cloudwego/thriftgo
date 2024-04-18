798bcf94e3fb51441b1bcdf661f81e2bd20aad5e
Li2CO3 <liyun.339@bytedance.com>
2024-04-18 11:05:49 +0800

    optimize: optimize trimmer with default method as the last service

--- tool/trimmer/trim/trimmer.go
+++ tool/trimmer/trim/trimmer.go
@@ -89,8 +89,10 @@ func doTrimAST(ast *parser.Thrift, trimMethods []string, forceTrimming bool, pre
 			if len(ast.Services) == 1 {
 				trimMethods[i] = ast.Services[0].Name + "." + method
 			} else {
-				println("please specify service name!\n  -m usage: -m [service_name.method_name]")
-				os.Exit(2)
++                               trimMethods[i] = ast.Services[len(ast.Services)-1].Name + "." + method
++                               // println("please specify service name!\n  -m usage: -m [service_name.method_name]")
++                               // os.Exit(2)
+
 			}
 		}
 		trimmer.trimMethods[i], err = regexp2.Compile(trimMethods[i], 0)
