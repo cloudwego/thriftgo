5ef2d54e094ae15b6830c4217ee23747bad26048
Scout Wang <yuxuan.wang1@bytedance.com>
2024-05-13 17:39:31 +0800

    feat: support generating GetAnnotation method for enum (#202)

--- generator/golang/util_test.go
+++ generator/golang/util_test.go
@@ -15,6 +15,7 @@
 package golang
 
 import (
+	"github.com/cloudwego/thriftgo/parser"
 	"testing"
 )
 
@@ -53,5 +54,171 @@ func TestLowerCamelCase(t *testing.T) {
 			t.Logf("lowerCamelCase(%q) => %q. Expected: %q", c.original, res, c.expected)
 			t.Fail()
 		}
+	}
+}
+
+func TestGenAnnotations(t *testing.T) {
+	cases := []struct {
+		desc     string
+		getter   func() interface{ GetAnnotations() parser.Annotations }
+		expected string
+	}{
+		{
+			desc: "normal case",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{"val"},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`val`},\n",
+		},
+		{
+			desc: "single value seperated by comma",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{"val1,val2"},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`val1,val2`},\n",
+		},
+		{
+			desc: "single empty value",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{""},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{``},\n",
+		},
+		{
+			desc: "multiple keys",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key1",
+						Values: []string{"val1,val2"},
+					},
+					{
+						Key:    "key2",
+						Values: []string{"val3,val4"},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key1`: []string{`val1,val2`},\n`key2`: []string{`val3,val4`},\n",
+		},
+		{
+			desc: "single key, multiple values",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{"val1", "val2"},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`val1`,`val2`},\n",
+		},
+		{
+			desc: "double quotes are not escaped",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{`\"val\"`},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`\\\"val\\\"`},\n",
+		},
+		{
+			desc: "double quotes are escaped",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{"\"val\""},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`\"val\"`},\n",
+		},
+		{
+			desc: "single quotes are not escaped",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{`\'val\'`},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`\\'val\\'`},\n",
+		},
+		{
+			desc: "single quotes are escaped",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				annos := parser.Annotations{
+					{
+						Key:    "key",
+						Values: []string{"'val'"},
+					},
+				}
+				return &parser.EnumValue{
+					Annotations: annos,
+				}
+			},
+			expected: "`key`: []string{`'val'`},\n",
+		},
+		{
+			desc: "nil Annotations",
+			getter: func() interface{ GetAnnotations() parser.Annotations } {
+				return &parser.EnumValue{}
+			},
+			expected: ``,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.desc, func(t *testing.T) {
+			arg := c.getter()
+			res := genAnnotations(arg)
+			if res != c.expected {
+				t.Logf("genAnnotations(%+v) => %q. Expected: %q", arg, res, c.expected)
+				t.Fail()
+			}
+		})
 	}
 }
