d3c3bda001b45aa281a22c29fcbc93749dc11bb3
HeyJavaBean <liyun.339@bytedance.com>
2024-05-31 10:58:08 +0800

    optimize: change enum range check from error to warning

--- semantic/checker.go
+++ semantic/checker.go
@@ -16,6 +16,7 @@ package semantic
 
 import (
 	"fmt"
+	"log"
 	"math"
 
 	"github.com/cloudwego/thriftgo/parser"
@@ -116,8 +117,7 @@ func (c *checker) CheckEnums(t *parser.Thrift) (warns []string, err error) {
 			}
 			// check if enum value can be safely converted to int 32
 			if v.Value < math.MinInt32 || v.Value > math.MaxInt32 {
-				err = fmt.Errorf("the value of enum %s is %d, which exceeds the range of int32", v.Name, v.Value)
-				return
+				log.Printf("the value of enum %s is %d, which exceeds the range of int32. Please adjust its value to fit within the int32 range to avoid data errors during serialization!!!\n", v.Name, v.Value)
 			}
 		}
 	}
