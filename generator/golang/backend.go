ee2a99a8e03cc1906fc87861df99d720f7ee57cb
HeyJavaBean <liyun.339@bytedance.com>
2025-02-17 14:51:44 +0800

    fix: fix trimmer when service is empty

--- generator/golang/backend.go
+++ generator/golang/backend.go
@@ -92,11 +92,13 @@ func (g *GoBackend) Generate(req *plugin.Request, log backend.LogFunc) *plugin.R
 		tr, err := trim.TrimAST(&trim.TrimASTArg{Ast: req.AST, TrimMethods: nil, Preserve: nil})
 		if err != nil {
 			g.log.Warn("trim error:", err.Error())
+			g.err = err
+			return g.buildResponse()
+		} else {
+			g.log.Warn(fmt.Sprintf("removed %d unused structures with %d fields", tr.StructsTrimmed, tr.FieldsTrimmed))
+
+			g.log.Warn(fmt.Sprintf("structs:%d->%d (%.1f%% Trimmed),  fields:%d->%d (%.1f%% Trimmed).", tr.StructsTotal, tr.StructsLeft(), tr.StructTrimmedPercentage(), tr.FieldsTotal, tr.FieldsLeft(), tr.FieldTrimmedPercentage()))
 		}
-
-		g.log.Warn(fmt.Sprintf("removed %d unused structures with %d fields", tr.StructsTrimmed, tr.FieldsTrimmed))
-
-		g.log.Warn(fmt.Sprintf("structs:%d->%d (%.1f%% Trimmed),  fields:%d->%d (%.1f%% Trimmed).", tr.StructsTotal, tr.StructsLeft(), tr.StructTrimmedPercentage(), tr.FieldsTotal, tr.FieldsLeft(), tr.FieldTrimmedPercentage()))
 	}
 	g.prepareTemplates()
 	g.fillRequisitions()
@@ -244,7 +246,6 @@ func (s *Scope) IsEmpty() bool {
 }
 
 func (g *GoBackend) renderByTemplate(scope *Scope, executeTpl *template.Template, filename string) error {
-
 	if scope == nil {
 		return nil
 	}
